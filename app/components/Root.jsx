4b66940a369f1c0a23661a1431eb0ce8f24ad42a
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-29 16:04:17 -0400

    Uses HashRouter; create StudentList component

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-// import {Route} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import Navbar from './Navbar'
 import CampusList from './CampusList'
+import StudentList from './StudentList'
 
 export default function Root(){
   return (
     <div id='app' className='container'>
       <Navbar />
-      <CampusList />
+      <Route path="/" exact={true} component={CampusList} />
+      <Route path="/campuses" component={CampusList} />
+      <Route path="/students" component={StudentList} />
     </div>
   )
 }
---
+++ app/components/StudentList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function CampusList(props){
+  return (
+    <table className="table table-condensed table-hover .table-bordered">
+      <tr>
+        <th>Name Namename</th>
+        <th>Campus</th>
+        <th><button className="btn btn-sm btn-danger">X</button></th>
+      </tr>
+      <tr>
+        <th>Name Namename</th>
+        <th>Campus</th>
+        <th><button className="btn btn-sm btn-danger">X</button></th>
+      </tr>
+    </table>
+  )
+}
