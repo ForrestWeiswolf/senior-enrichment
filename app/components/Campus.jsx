bfe7f8ea6201af828f540cfd5e4011dc1ed41336
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-08-30 16:54:09 -0400

    replace <a> tag with Link component

--- app/components/Campus.jsx
+++ app/components/Campus.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Campus(props){
   return (
     <div className="col-md-4">
-      <a href={`/campuses/${props.campus.id}`}>
+      <Link to={`/campuses/${props.campus.id}`}>
           <img src={props.campus.image} className="img-responsive img-rounded"></img>
           <h2>{props.campus.name}</h2>
-      </a>
+      </Link>
     </div>
   )
 }
